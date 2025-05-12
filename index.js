564b5c6a7fce8d9e06a85c3aeb1a0889642f1a00
Ritika1223 <ritichaudhary112@gmail.com>
2025-05-12 23:40:07 +0530

    update

--- index.js
+++ index.js
@@ -8,13 +8,10 @@ const cookieParser = require('cookie-parser')
 const authRoutes = require('./routes/auth');
 const operatorRoutes = require('./routes/operators');
 
-// Use your deployed frontend domain in production
-const allowedOrigin = process.env.FRONTEND_URL || 'https://antbus.in/';
-
 app.use(cookieParser()) 
 app.use(cors({
   credentials:true,
-  origin: allowedOrigin
+  origin: 'https://antbus.in'
 }));
 app.use(express.json())
 
